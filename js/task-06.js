7ba220a3cfaa665fe24618d358413bed8df8a570
DanyloTytarenko <121917694+DanyloTytarenko@users.noreply.github.com>
2023-04-29 15:03:54 +0300

    fix

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const input = document.getElementById("validation-input");
 const correct = input.getAttribute("data-length");
 input.addEventListener("blur", () => {
-  if (input.value.length >= Number(correct)) {
+  if (input.value.length === Number(correct)) {
     input.classList.remove("invalid");
     input.classList.add("valid");
   } else {
--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const input = document.getElementById("font-size-control");
 const text = document.getElementById("text");
-input.addEventListener("input", () => {
-  text.style.fontSize = `${input.value}px`;
+input.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  text.style.fontSize = `${e.target.value}px`;
 });
